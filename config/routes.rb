a29ac65c10c4dbc31f94ad25c57eb6715f954076
loc-cv <donbb6824@gmail.com>
2023-01-17 14:28:47 +0700

    Create User table with Devise generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, path: "auth", path_names: { sign_in: :login, sign_out: :logout, sign_up: :register }
 end
