61a32145867a77833cd1f8d73d2949d49e828fa3
loc-cv <donbb6824@gmail.com>
2023-01-17 16:02:26 +0700

    Configure Message model to enable real time updating

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,7 @@ class Message < ApplicationRecord
   belongs_to :room
 
   validates :body, presence: true
+
+  # Send messages with real time update
+  after_create_commit { broadcast_append_to room }
 end
