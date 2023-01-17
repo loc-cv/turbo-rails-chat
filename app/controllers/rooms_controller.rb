0264e3e4b9f11da7517d559497b55e4fc4ea91d3
loc-cv <donbb6824@gmail.com>
2023-01-17 15:11:43 +0700

    Render rooms and users in Rooms#index view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @public_rooms = Room.public_rooms
+    @other_users = User.all_except(current_user)
   end
 end
