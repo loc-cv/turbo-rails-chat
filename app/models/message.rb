87b0a3c6fab417f445360d111060e4312fcfe566
loc-cv <donbb6824@gmail.com>
2023-01-17 21:17:58 +0700

    Add Message callback to make sure messages can't be sent if sender is not a participant of a private room

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,17 @@ class Message < ApplicationRecord
 
   # Send messages with real time update
   after_create_commit { broadcast_append_to room }
+
+  # Make sure messages can't be sent (created) if sender is not a participant ...
+  # ... of a private room even in the console.
+  before_create :confirm_participant
+
+  private
+
+    def confirm_participant
+      return unless room.is_private?
+
+      is_participant = Participant.find_by(user_id: user.id, room_id: room.id)
+      throw :abort unless is_participant
+    end
 end
