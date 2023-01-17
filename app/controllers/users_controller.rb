3a07796a417cb1764669702c9c63b5c898c2eb26
loc-cv <donbb6824@gmail.com>
2023-01-17 21:12:33 +0700

    Create users_controller with 'show' action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :set_public_rooms, only: [:show]
+  before_action :set_other_users, only: [:show]
+
+  def show
+    @room = Room.new
+    @message = Message.new
+    @user = User.find(params[:id])
+    @private_room_name = get_private_room_name(@user, current_user)
+    @current_room = Room.find_by(name: @private_room_name) ||
+                    Room.create_private_room([@user, current_user], @private_room_name)
+    @messages = @current_room.messages.order(created_at: :asc)
+    render "rooms/index"
+  end
+
+  private
+
+    def get_private_room_name(first_user, second_user)
+      users = [first_user, second_user].sort
+      "private_#{users[0].id}_#{users[1].id}"
+    end
+
+    def set_public_rooms
+      @public_rooms = Room.public_rooms
+    end
+
+    def set_other_users
+      @other_users = User.all_except(current_user)
+    end
+end
--- app/models/room.rb
+++ app/models/room.rb
@@ -9,6 +9,14 @@ class Room < ApplicationRecord
   # Real time update the rooms list in the sidebar when a new PUBLIC room is created.
   after_create_commit { broadcast_if_public }
 
+  def self.create_private_room(users, room_name)
+    room = Room.create(name: room_name, is_private: true)
+    users.each do |user|
+      Participant.create(user_id: user.id, room_id: room.id)
+    end
+    room
+  end
+
   private
 
     def broadcast_if_public
