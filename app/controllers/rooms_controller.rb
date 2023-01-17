35c0519504f801e89657b5f7e4c32e74aab7a538
loc-cv <donbb6824@gmail.com>
2023-01-17 16:03:40 +0700

    Create 'show' action for RoomsController

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,18 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_public_rooms, only: %i[index show]
+  before_action :set_other_users, only: %i[index show]
 
   def index
     @room = Room.new
-    @public_rooms = Room.public_rooms
-    @other_users = User.all_except(current_user)
+  end
+
+  def show
+    @room = Room.new
+    @message = Message.new
+    @current_room = Room.find(params[:id])
+    @messages = @current_room.messages.order(created_at: :asc)
+    render "index"
   end
 
   def create
@@ -15,5 +23,13 @@ class RoomsController < ApplicationController
 
     def room_params
       params.require(:room).permit(:name)
+    end
+
+    def set_public_rooms
+      @public_rooms = Room.public_rooms
+    end
+
+    def set_other_users
+      @other_users = User.all_except(current_user)
     end
 end
