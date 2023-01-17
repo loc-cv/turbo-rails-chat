aeaed8bfa6fd0bd11532a3ee7ad7a678fd6464ae
loc-cv <donbb6824@gmail.com>
2023-01-17 15:23:30 +0700

    Real time users list and rooms list update in the sidebar

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
   scope :public_rooms, -> { where(is_private: false) }
+
+  # Real time update the rooms list in the sidebar when a new room is created.
+  after_create_commit { broadcast_append_to "rooms", partial: "rooms/room_navlink" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   scope :all_except, ->(user) { where.not(id: user) }
+
+  # Real time update the users list in the sidebar when a new user signs up.
+  after_create_commit { broadcast_append_to "users", partial: "users/user_navlink" }
 end
