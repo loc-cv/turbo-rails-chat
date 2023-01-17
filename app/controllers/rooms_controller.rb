a3dd15a016e727b36645b6418f99b43d9df32ff3
loc-cv <donbb6824@gmail.com>
2023-01-17 15:38:51 +0700

    Create form partial to create new public room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,18 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @room = Room.new
     @public_rooms = Room.public_rooms
     @other_users = User.all_except(current_user)
   end
+
+  def create
+    @room = Room.create(room_params)
+  end
+
+  private
+
+    def room_params
+      params.require(:room).permit(:name)
+    end
 end
